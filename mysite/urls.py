baa798ac8d3cd14e98376c96a58d1200c133f6d5
stantheguru <sirstanleyzurki@gmail.com>
2023-03-12 22:09:13 +0300

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include     # <--  ADD THIS LINE
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('hello_world.urls')),   # <--  ADD THIS LINE
+]
